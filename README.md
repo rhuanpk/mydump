df270b9a1215ff4d3ed192c0eb2a684c52fcfe89
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-04-01 10:52:28 -0300

    add: README.md; install.sh: atualizado

---
+++ README.md
@@ -0,0 +1,6 @@
+# MyDump
+
+## Install
+~~~bash
+wget -qO - "https://raw.githubusercontent.com/rhuan-pk/mydump/master/install.sh" | bash -
+~~~
--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 # urxvt -bg black -fg white +sb -fn 10x20 -bc -uc -lsp 1 -e 'mydump'
 mydump_path='/opt/mydump'
 sudo apt install ssh openssh-server sshpass terminator -y
-git clone 'https://github.com/rhuan-pk/mydump.git' "${mydump_path}"
+sudo git clone 'https://github.com/rhuan-pk/mydump.git' "${mydump_path}"
 sudo ln -s ${mydump_path}/mydump.sh /usr/local/bin/mydump
 sudo ln -s ${mydump_path}/mydump-start.sh /usr/local/bin/mydump-start
-cat << EOF > /home/${USER}/.local/share/applications/mydump.desktop
+sudo cat << EOF > /home/${USER}/.local/share/applications/mydump.desktop
 [Desktop Entry]
         Encoding=UTF-8
         Name=MyDump
