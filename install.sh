8c7a8e268608c17ba2c1e1ef937f76cf3d3fd569
Rhuan Patriky <rhuan.pksf@gmail.com>
2022-04-01 10:45:48 -0300

    add: mydump-start.sh; refreshado install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 # sudo wget "https://xfonts.pro/xfonts_files/5e4ba999a0eae905e9ddddb5/files/JetBrainsMono-Regular.ttf"
 # /usr/share/terminology/fonts - dar o wget aqui dentro
+# urxvt -bg black -fg white +sb -fn 10x20 -bc -uc -lsp 1 -e 'mydump'
 mydump_path='/opt/mydump'
+sudo apt install ssh openssh-server sshpass terminator -y
 git clone 'https://github.com/rhuan-pk/mydump.git' "${mydump_path}"
 sudo ln -s ${mydump_path}/mydump.sh /usr/local/bin/mydump
-sudo ln -s ${mydump_path}/refresh.sh /usr/local/bin/mydump-refresh
-cat << EOF > /home/${USER}/.local/share/applications/
+sudo ln -s ${mydump_path}/mydump-start.sh /usr/local/bin/mydump-start
+cat << EOF > /home/${USER}/.local/share/applications/mydump.desktop
 [Desktop Entry]
         Encoding=UTF-8
         Name=MyDump
         Icon=${mydump_path}/icons/logo.png
-        Exec=/
+        Exec=mydump-start
         Terminal=false
         Type=Application
-        Categories=Desenvolvimento
 EOF
+sudo chmod +x /home/${USER}/.local/share/applications/mydump.desktop
---
+++ mydump-start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+terminator --borderless --maximise --command='mydump; bash'
